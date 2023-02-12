fa6e628996d37c234e34ea16b2fb4a48102e08e2
yewe <khalid.yewe@gmail.com>
2023-02-12 19:45:56 +0300

    used cors

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const appRoutes = require("./routes/appRoutes");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(appRoutes);
 
